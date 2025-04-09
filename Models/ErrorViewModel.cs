6ce21643eb9a5d8b4651149552fcb02879c1d15d
EvanGrone <106884776+EvanGrone@users.noreply.github.com>
2025-04-09 11:01:30 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokemonCardCollector.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
