c02c5c1d280a6e5fc13adb4791f5dae140f0c8fe
EvanGrone <106884776+EvanGrone@users.noreply.github.com>
2025-04-11 22:28:48 -0400

    Added CSS and Javascript Validation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cardForm = "#cardForm";
+    const collectionForm = "#collectionForm";
+
+    if (cardForm) {
+        cardForm.addEventListener("submit", function (event) {
+            const name = document.getElementById("Name").value;
+            const SetName = document.getElementById("SetName").value;
+            const SetNumber = document.getElementById("SetNumber").value;
+            const type = document.getElementById("Type").value;
+            const Price = document.getElementById("Price").value;
+
+            if (!name || !SetName || !SetNumber || !type || !Price) {
+                event.preventDefault();
+                alert("Please fill in all fields.");
+            }
+        });
+    }
+
+    if (collectionForm) {
+        collectionForm.addEventListener("submit", function (event) {
+            const name = document.getElementById("Name").value;
+            const Description = document.getElementById("Description").value;
+            const CreatedDate = document.getElementById("CreatedDate").value;
+            if (!name || !Description || !CreatedDate) {
+                event.preventDefault();
+                alert("Please fill in all fields.");
+            }
+        });
+    }
+});
+
+   
